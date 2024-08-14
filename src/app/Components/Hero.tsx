b0896332f4034b95059c5e304dd6716631abf6c0
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-08-14 09:02:21 +0100

    title Animation

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import Dive from "./Dive";
 import Search from "./Search";
+import { motion } from "framer-motion"; // Import framer-motion
 
 import { TourGuideContext } from "../context/tourGuideContext"; // Import the context
 import { useRouter } from "next/navigation";
@@ -49,19 +50,28 @@ const Hero: React.FC = () => {
         />
         <div className="md:h-[32.75rem] realtive top-0 pt-[9rem] md:pt-[10rem] pb-[5rem] md:pb-[5rem] bg-cover bg-gradient-to-r from-green-900 to-orange-800">
           <div className="relative top-[2.75rem] md:top-[4.75rem] flex flex-col justify-center text-center font-bold text-white">
-            <div>
-              <h1
-                id="hero-title1"
-                className="text-[5rem] md:text-[10.5rem] tracking-normal"
-              >
+            <motion.div
+              id="Titel"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 2.3 }}
+              className="text-[5rem] md:text-[10.5rem] tracking-normal"
+            >
+              <h1>
                 Dis
                 <span className="text-border">co</span>
                 ver
               </h1>
-            </div>
-            <div className="text-[2.5rem] md:text-[3.3rem] mt-[-0.5rem] tracking-tighter md:tracking-normal relative top-[-2rem] md:top-0 md:mt-[-4.5rem] font-normal">
-              <h1 id="hero-title2">the wonders of Côte d&apos;Ivoire</h1>
-            </div>
+            </motion.div>
+            <motion.div
+              id="hero-title2"
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 2.65 }}
+              className="text-[2.5rem] md:text-[3.3rem] mt-[-0.5rem] tracking-tighter md:tracking-normal relative top-[-2rem] md:top-0 md:mt-[-4.5rem] font-normal"
+            >
+              <h1>the wonders of Côte d&apos;Ivoire</h1>
+            </motion.div>
           </div>
         </div>
       </div>
@@ -70,12 +80,3 @@ const Hero: React.FC = () => {
 };
 
 export default Hero;
-
-{
-  /* <Search
-              setLocation={setLocationState}
-              setTourGuideName={setTourGuideNameState}
-              location={location}
-              tourGuideName={tourGuideName}
-            /> */
-}
